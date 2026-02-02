989021880c865eec8bdfdb3c498b7bda848b7706
unknown <imsonukalwar@gmail.com>
2026-02-02 13:59:11 +0530

    final code

--- back-end/emailverify/verifyEmail.js
+++ back-end/emailverify/verifyEmail.js
@@ -1,44 +1,75 @@
 
 
-const nodemailer=require('nodemailer')
-require('dotenv').config();
+// const nodemailer=require('nodemailer')
+// require('dotenv').config();
 
 
-const verifyemail=async(token,email)=>{
-        console.log("kam to kar raha hau");
-const transporter=nodemailer.createTransport({
+// const verifyemail=async(token,email)=>{
+//         console.log("kam to kar raha hau");
+// const transporter=nodemailer.createTransport({
 
     
-    // service:'email',
+//     // service:'email',
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth:{
+//         user:process.env.MAIL_USER,
+//         pass:process.env.MAIL_PASS,
+//     },
+//   tls: {
+//     rejectUnauthorized: false
+//   }
+// })
+// const mailConfiguration={
+//     from:process.env.MAIL_USER,
+//     to:email,
+//     subject:'Email.Verification',
+//     text:`Hi ! There,  You have recently visited 
+//     our website and entiredyour email.please follow 
+//     the given link to verify your email
+//     ${process.env.CLIENT_URL}/verify/${token}
+//     thanks `
+// }
+// await transporter.sendMail(mailConfiguration,function(error,info){
+//     if(error){
+//         throw new Error(error);
+//     }
+//     console.log("email send sucsessfully !");
+//     console.log(info);
+// })
+// }
+
+// module.exports=verifyemail;
+
+
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const verifyemail = async (token, email) => {
+  const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    auth:{
-        user:process.env.MAIL_USER,
-        pass:process.env.MAIL_PASS,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
     },
-  tls: {
-    rejectUnauthorized: false
-  }
-})
-const mailConfiguration={
-    from:process.env.MAIL_USER,
-    to:email,
-    subject:'Email.Verification',
-    text:`Hi ! There,  You have recently visited 
-    our website and entiredyour email.please follow 
-    the given link to verify your email
-    ${process.env.CLIENT_URL}/verify/${token}
-    thanks `
-}
-await transporter.sendMail(mailConfiguration,function(error,info){
-    if(error){
-        throw new Error(error);
-    }
-    console.log("email send sucsessfully !");
-    console.log(info);
-})
-}
+  });
 
+  const mailConfiguration = {
+    from: process.env.MAIL_USER,
+    to: email,
+    subject: "Email Verification",
+    html: `
+      <h3>Verify Your Email</h3>
+      <a href="${process.env.BASE_URL}/verify/${token}">
+        Click Here To Verify
+      </a>
+    `,
+  };
 
-module.exports={verifyemail};
+  await transporter.sendMail(mailConfiguration);
+};
+
+module.exports = { verifyemail };
